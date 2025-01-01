44599109bfa59c33bf1560848a303946f47ba371
shahmaran0207 <shahmaran0207@naver.com>
2025-01-01 12:46:54 +0900

    QnA - Question detail proceeding2

--- src/main/java/com/JPA/Member/Controller/QnA/QuestionController.java
+++ src/main/java/com/JPA/Member/Controller/QnA/QuestionController.java
@@ -1,5 +1,7 @@
 package com.JPA.Member.Controller.QnA;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.JPA.Member.Service.QnA.Question.QuestionService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import java.util.List;
 
-@Controller("/QnA/Question")
+@Controller
+@RequestMapping("/QnA/Question")
 @RequiredArgsConstructor
 public class QuestionController {
     private final QuestionService questionService;
@@ -21,7 +24,6 @@ public class QuestionController {
     }
 
     @GetMapping("/list")
-    @ResponseBody
     public String QnAList(Model model) {
         List<QuestionEntity> questionEntityList = questionService.findAll();
 
@@ -31,8 +33,7 @@ public class QuestionController {
     }
 
     @GetMapping("/detail")
-    @ResponseBody
-    public String QuestionDetail() {
+    public String QuestionDetail(Model model, @PathVariable("id") Integer id) {
         return "/QnA/Question/detail";
     }
 }
